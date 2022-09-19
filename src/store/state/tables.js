9b6b4445b1715dacda1fb5640966719bfd016987
geo <tp53.geo@gmail.com>
2022-09-19 09:07:42 +0200

    ajout data pour exploiter toutes les fonctionnalités

--- src/store/state/tables.js
+++ src/store/state/tables.js
@@ -1,23 +1,45 @@
 export const tablesInitialState = [
   {
+    id: "T0",
+    kanbanId: "KJ6786HKE",
+    title: "Conception",
+    color: "#3fa3b3",
+    description:
+      "Ce qu'il faut cenceptualiser à l'aide d'UML et divers documents",
+  },
+  {
     id: "T1",
     kanbanId: "KJ6786HKE",
-    title: "A faire",
+    title: "A Réaliser",
     color: "#7F84B4",
-    description: "A small description of table",
+    description: "Ce qu'il faut implémenter.",
   },
   {
     id: "T2",
     kanbanId: "KJ6786HKE",
     title: "En cours",
     color: "#B556A6",
-    description: "A small description of table",
+    description: "Ce qui est en train d'être réalisé actuellement",
   },
   {
     id: "T3",
     kanbanId: "KJ6786HKE",
-    title: "Terminé",
+    title: "Validation",
+    color: "#3C5A95",
+    description: "Ce qui a besoin d'être révisé et validé par le chef d'équipe",
+  },
+  {
+    id: "T4",
+    kanbanId: "KJ6786HKE",
+    title: "A tester",
     color: "#C85748",
-    description: "A small description of table",
+    description: "Ce qui doit être testé avant mise en production",
+  },
+  {
+    id: "T5",
+    kanbanId: "KJ6786HKE",
+    title: "Terminé",
+    color: "#4FA24D",
+    description: "Ce qui est terminé et prêt à etre déployé.",
   },
 ];
